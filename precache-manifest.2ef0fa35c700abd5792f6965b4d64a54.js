0c7786e12a66e316be7e9ba12b1bec62417e5635
Debbie <1770554944@qq.com>
2021-04-14 17:45:36 +0800

    deploy

---
+++ precache-manifest.2ef0fa35c700abd5792f6965b4d64a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3066f019683650cbe407",
+    "url": "/cherryaccount-website/css/app.e8f1e23c.css"
+  },
+  {
+    "revision": "36b9e0f3404a610a61faf4f19af3e73f",
+    "url": "/cherryaccount-website/index.html"
+  },
+  {
+    "revision": "3066f019683650cbe407",
+    "url": "/cherryaccount-website/js/app.96b5ec0e.js"
+  },
+  {
+    "revision": "e2ec734148e30ea882c5",
+    "url": "/cherryaccount-website/js/chunk-vendors.f529e0d3.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/cherryaccount-website/manifest.json"
+  },
+  {
+    "revision": "eb73a6c5bc9eda030887aea936328e67",
+    "url": "/cherryaccount-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cherryaccount-website/robots.txt"
+  }
+]);
